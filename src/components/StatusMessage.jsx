486d4eef933543f189e63aa9e4a5b4511f58ec52
Rangan Das <87185474+developerRangan@users.noreply.github.com>
2022-06-18 00:23:17 +0530

    Add files via upload

---
+++ src/components/StatusMessage.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const StatusMessage = ({winner,current}) => {
+
+    const noMovesLeft = current.board.every(el => el !== null);
+  
+  return (
+    <div className="status-message"> 
+        {winner && (
+        <>
+        Winner is {' '}
+        <span classname={winner === 'X' ? 'text-green' : 'text-orange'}>
+            {winner}</span>
+        </>
+        )}
+        {!winner && !noMovesLeft && (
+        <>
+        Next player is {' '}
+        <span className={current.isXNext ? 'text-green' : 'text-orange'}>
+            {current.isXNext ? 'X' : 'O'}{' '}
+            </span>
+        </>
+        )}
+        {!winner && noMovesLeft && <>
+        <span classname="text-green">X</span> and <span className="text-orange">O</span> tied
+        </>
+        }
+
+    </div>
+  );
+};
+
+export default StatusMessage
